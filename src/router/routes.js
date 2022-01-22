3cdf006f3f9c293ce2ead9507c2fbde1b84e38ff
Robson Nascimento <nascimento.robson12@gmail.com>
2022-01-22 12:36:26 -0300

    feature services

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,9 @@
+
 import { routes as alltypes } from '@/views/Registers/AllTypes';
 import { routes as auth } from '@/views/Auth';
 import { routes as home } from '@/views/Home/Dashboard';
 import { routes as registers } from '@/views/Registers/Registers';
-import { routes as settings } from '@/views/Settings/Companies';
+import { routes as companies } from '@/views/Settings/Companies';
 import { routes as users } from '@/views/Settings/Users';
 import { routes as services } from '@/views/Services';
 
@@ -13,7 +14,7 @@ export default [
   ...auth,
   ...home,
   ...registers,
-  ...settings,
+  ...companies,
   ...services,
   ...users,
 ]
