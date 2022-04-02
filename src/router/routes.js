a11b5caed4030f4d707dc2d83ed64793f3e9588f
Renan Reis <renan.reis@locaweb.com.br>
2022-04-02 13:49:16 -0300

    feat(app): refactor imports routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,13 @@
 
-import { routes as alltypes } from '@/views/Registers/AllTypes';
+import { routes as allTypes } from '@/views/Registers/AllTypes';
 import { routes as appointments } from '@/views/Operations/Appointments';
 import { routes as auth } from '@/views/Auth';
-import { routes as accountpayments } from '@/views/Financial/AccountPayments';
+import { routes as accountPayments } from '@/views/Financial/AccountPayments';
 import { routes as banks } from '@/views/Financial/Banks';
 import { routes as boxes } from '@/views/Financial/Boxes';
 import { routes as calendars } from '@/views/Operations/Calendars';
 import { routes as companies } from '@/views/Settings/Companies';
-import { routes as installmenttypes } from '@/views/Registers/InstallmentTypes';
+import { routes as installmentTypes } from '@/views/Registers/InstallmentTypes';
 import { routes as home } from '@/views/Home/Dashboard';
 import { routes as orderServices } from '@/views/Operations/OrderServices';
 import { routes as registers } from '@/views/Registers/Registers';
@@ -16,15 +16,15 @@ import { routes as users } from '@/views/Settings/Users';
 
 
 export default [
-  ...alltypes,
+  ...allTypes,
   ...appointments,
   ...auth,
-  ...accountpayments,
+  ...accountPayments,
   ...banks,
   ...boxes,
   ...calendars,
   ...companies,
-  ...installmenttypes,
+  ...installmentTypes,
   ...home,
   ...orderServices,
   ...registers,
